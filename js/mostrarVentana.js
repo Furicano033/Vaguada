56df347738463febfd57c60ddb4ba92c61b2082c
Juan Esteban López <jestebanl1@alumnos.nebrija.es>
2023-12-05 10:51:00 +0100

    hola

---
+++ js/mostrarVentana.js
@@ -0,0 +1,13 @@
+
+
+      //JAVA que es un lenguaje fuertemente tipado(Cuando pongo una variable tengo que darle un tipo)
+      
+function mostrarVentana(){  
+    document.getElementById("formulario").style.display = "block";
+    document.getElementById("cuerpo").style.filter = "blur"; 
+}
+function cerrarFormulario(){
+    document.getElementById("formulario").style.display = "none";
+    document.getElementById("cuerpo").style.filter = "none";
+}
+    
